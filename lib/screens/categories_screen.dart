a2b6958cf8cd33509dda002b9b44a88882c721a1
ayesh <ayeshlak1998@gmail.com>
2021-04-21 14:16:19 +0530

    Implemented category item component and used it in category screen to load categories.

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:meals_flutter_app/components/category_item.dart';
+import 'package:meals_flutter_app/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: <Widget>[],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Meal'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+      ),
     );
   }
 }
